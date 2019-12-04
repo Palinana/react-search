36a66f616d1ac76c7005155568e405bc838438a5
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2019-12-04 18:13:04 -0500

    Added infinite scroll

---
+++ InfiniteScroll/client/README.md
@@ -0,0 +1,2 @@
+To start the project directory, you can run this command in /client:
+### `npm start`
---
+++ InfiniteScroll/secrets.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || '051ad1f704f43a3a6e49378c05e3eb4afd550c897de6ba15cad23e20fd920ee7',
+    SECRET: process.env.SECRET || '3e961d3733464e15964f7587e2946c367be3215facc820c3e856dbbacea98c65',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+};
